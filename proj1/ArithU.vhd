f189f4fad2e665fd80ac5aff2545b19308a72485
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-03-12 13:09:24 +0000

    Corrected ArithU. Now need to rework ALU.vhd.

--- proj1/ArithU.vhd
+++ proj1/ArithU.vhd
@@ -46,24 +46,27 @@ end ArithU;
 
 architecture Behavioral of ArithU is
 
-	signal bufferout: std_logic_vector(16 downto 0);
+	signal bufferout : std_logic_vector(16 downto 0);
+	signal A_ext : std_logic_vector(16 downto 0);
+	signal B_ext : std_logic_vector(16 downto 0);
 
 begin
 
+	A_ext <= A1(15) & A1;
+	B_ext <= B1(15) & B1;
+
 	with sel(2 downto 0) select bufferout <=
-	A1+B1 when "000",
-	A1+B1+1 when "001",
-	A1+1 when "011",
-	A1-B1-1 when "100",
-	A1-B1 when "101",
-	A1-1 when others;
+	A_ext+B_ext when "000",
+	A_ext+B_ext+1 when "001",
+	A_ext+1 when "011",
+	A_ext-B_ext-1 when "100",
+	A_ext-B_ext when "101",
+	A_ext-1 when others;
 
-	with sel(2) select flag_v <=	-- /!\ check this
-	(((A1(15) nor B1(15)) and bufferout(15)) or ((A1(15) and B1(15)) and (not bufferout(15)) )) when '0',
-	((((not A1(15)) and B1(15)) and bufferout(15)  ) or ( ((not B1(15)) and A1(15)) and (not bufferout(15)))  ) when others;
 
+	flag_v <= (A_ext(15) and B_ext(15) and not bufferout(15)) or (not A_ext(15) and not B_ext(15) and bufferout(15));
 	flag_z <= '1' when bufferout(15 downto 0) = X"0000" else '0';
-	flag_c <= bufferout(16);
+	flag_c <= bufferout(16);	-- carry makes no sense for signed addition
 	flag_s <= bufferout(15);
 
 	AOut <= bufferout(15 downto 0);
