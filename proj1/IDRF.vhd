172f1fd0a4e7167a4bca4ebdfccd832b64df7dee
raffitz <raf.a.m.c.gon@gmail.com>
2015-03-22 01:24:12 +0000

    Fixed the relative jump situation

--- proj1/IDRF.vhd
+++ proj1/IDRF.vhd
@@ -110,7 +110,8 @@ begin
 	
 	
 
-	ALU_op <= "10011" when inst(15 downto 14) = "00" else -- jumps
+	ALU_op <= "10011" when inst(15 downto 13) = "001" else -- absolute jumps
+		"00000" when inst(15 downto 14) = "00" else -- relative jumps
 		inst(10 downto 6);
 	mux_a <= '1' when inst(15 downto 14) = "00" else	-- control transfer
 		'0'; -- Possibly overly simplistic
