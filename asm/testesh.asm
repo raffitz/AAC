8b45b86372fcfb17227ee9cefe486aa444fe44dd
raffitz <raf.a.m.c.gon@gmail.com>
2015-03-23 11:48:40 +0000

    Fixed minor bug in shift test

--- asm/testesh.asm
+++ asm/testesh.asm
@@ -7,16 +7,16 @@ main:
 		loadlit r0, 1
 		asr r0, r0	; C, Z (lowest bit was one and was dropped off)
 		asr r0, r0	; Z
-		lch r0, 128
-		asr r0, r0	; none
-		lsl r0, r0	; none
-		lsl r0, r0	; S
+		lch r0, 64	; 0x4000
+		asr r0, r0	; none (expected result: 0x2000)
+		lsl r0, r0	; none 0x4000
+		lsl r0, r0	; S 0x8000
 		asr r0, r0	; S. r0 should now have 0xC000
 		lsl r0, r0	; S, C (highest bit was one and was dropped off)
 		lsl r1, r0	; C, Z
 
 		loadlit r0, 2
-		asr r0, r0	; none
+		asr r0, r0	; none (1)
 		asr r0, r0	; C, Z (lowest bit was one and was dropped off)
 		lsl r0, r0	; Z
 
