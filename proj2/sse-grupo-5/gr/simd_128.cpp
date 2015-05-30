2e3431931b6dba5cd5208e52ed25a80fe0db407b
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-05-30 18:44:25 +0000

    Added unroll8 version.

--- proj2/sse-grupo-5/gr/simd_128.cpp
+++ proj2/sse-grupo-5/gr/simd_128.cpp
@@ -371,6 +371,178 @@ void sse128_smoothing_unroll4(float *x, float *y, float *res, int len)
 }
 
 
+void sse128_smoothing_unroll8(float *x, float *y, float *res, int len)
+{
+	float *xi, *xj, *yj;
+	float aux[4];
+	float sumAtot, sumBtot;
+
+	__m128 sumA, sumB, sumA_1, sumB_1, sumA_2, sumB_2, sumA_3, sumB_3;
+	__m128 sumA_4, sumB_4, sumA_5, sumB_5, sumA_6, sumB_6, sumA_7, sumB_7;
+	__m128 divisor = _mm_div_ps(_mm_set_ps1(1.0), _mm_set_ps1(2*_SMOOTH*_SMOOTH));
+	__m128 exponential, exponential_1, exponential_2, exponential_3;
+	__m128 exponential_4, exponential_5, exponential_6, exponential_7;
+	__m128 cache, cache_1, cache_2, cache_3;
+	__m128 cache_4, cache_5, cache_6, cache_7;
+
+	for(xi=x; xi < x+len; xi++, res++)
+	{
+		sumA = sumB = _mm_setzero_ps();
+		sumA_1 = sumB_1 = _mm_setzero_ps();
+		sumA_2 = sumB_2 = _mm_setzero_ps();
+		sumA_3 = sumB_3 = _mm_setzero_ps();
+		sumA_4 = sumB_4 = _mm_setzero_ps();
+		sumA_5 = sumB_5 = _mm_setzero_ps();
+		sumA_6 = sumB_6 = _mm_setzero_ps();
+		sumA_7 = sumB_7 = _mm_setzero_ps();
+
+		for(xj=x, yj=y; xj < x+len; xj+=32, yj+=32)
+		{
+			// e^[(-(xi-xj)^2) / (2*smoothing^2)]
+			cache = _mm_sub_ps(_mm_load_ps1(xi), _mm_load_ps(xj));
+			cache_1 = _mm_sub_ps(_mm_load_ps1(xi+4), _mm_load_ps(xj+4));
+			cache_2 = _mm_sub_ps(_mm_load_ps1(xi+8), _mm_load_ps(xj+8));
+			cache_3 = _mm_sub_ps(_mm_load_ps1(xi+12), _mm_load_ps(xj+12));
+			cache_4 = _mm_sub_ps(_mm_load_ps1(xi+16), _mm_load_ps(xj+16));
+			cache_5 = _mm_sub_ps(_mm_load_ps1(xi+20), _mm_load_ps(xj+20));
+			cache_6 = _mm_sub_ps(_mm_load_ps1(xi+24), _mm_load_ps(xj+24));
+			cache_7 = _mm_sub_ps(_mm_load_ps1(xi+28), _mm_load_ps(xj+28));
+
+			exponential = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache,
+								cache
+								)), divisor));
+
+			sumB = _mm_add_ps(sumB, exponential);
+			sumA = _mm_add_ps(sumA, _mm_mul_ps(_mm_load_ps(yj), exponential));
+
+
+
+			exponential_1 = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache_1,
+								cache_1
+								)), divisor));
+
+			sumB_1 = _mm_add_ps(sumB_1, exponential_1);
+			sumA_1 = _mm_add_ps(sumA_1, _mm_mul_ps(_mm_load_ps(yj+4), exponential_1));
+
+
+
+			exponential_2 = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache_2,
+								cache_2
+								)), divisor));
+
+			sumB_2 = _mm_add_ps(sumB_2, exponential_2);
+			sumA_2 = _mm_add_ps(sumA_2, _mm_mul_ps(_mm_load_ps(yj+8), exponential_2));
+
+
+
+			exponential_3 = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache_3,
+								cache_3
+								)), divisor));
+
+			sumB_3 = _mm_add_ps(sumB_3, exponential_3);
+			sumA_3 = _mm_add_ps(sumA_3, _mm_mul_ps(_mm_load_ps(yj+12), exponential_3));
+
+
+
+			exponential_4 = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache_4,
+								cache_4
+								)), divisor));
+
+			sumB_4 = _mm_add_ps(sumB_4, exponential_4);
+			sumA_4 = _mm_add_ps(sumA_4, _mm_mul_ps(_mm_load_ps(yj+16), exponential_4));
+
+
+
+			exponential_5 = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache_5,
+								cache_5
+								)), divisor));
+
+			sumB_5 = _mm_add_ps(sumB_5, exponential_5);
+			sumA_5 = _mm_add_ps(sumA_5, _mm_mul_ps(_mm_load_ps(yj+20), exponential_5));
+
+
+
+			exponential_6 = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache_6,
+								cache_6
+								)), divisor));
+
+			sumB_6 = _mm_add_ps(sumB_6, exponential_6);
+			sumA_6 = _mm_add_ps(sumA_6, _mm_mul_ps(_mm_load_ps(yj+24), exponential_6));
+
+
+
+			exponential_7 = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache_7,
+								cache_7
+								)), divisor));
+
+			sumB_7 = _mm_add_ps(sumB_7, exponential_7);
+			sumA_7 = _mm_add_ps(sumA_7, _mm_mul_ps(_mm_load_ps(yj+28), exponential_7));
+		}
+
+		sumA = _mm_add_ps(sumA, sumA_7);
+		sumB = _mm_add_ps(sumB, sumB_7);
+
+		sumA = _mm_add_ps(sumA, sumA_6);
+		sumB = _mm_add_ps(sumB, sumB_6);
+
+		sumA = _mm_add_ps(sumA, sumA_5);
+		sumB = _mm_add_ps(sumB, sumB_5);
+
+		sumA = _mm_add_ps(sumA, sumA_4);
+		sumB = _mm_add_ps(sumB, sumB_4);
+
+		sumA = _mm_add_ps(sumA, sumA_3);
+		sumB = _mm_add_ps(sumB, sumB_3);
+
+		sumA = _mm_add_ps(sumA, sumA_2);
+		sumB = _mm_add_ps(sumB, sumB_2);
+
+		sumA = _mm_add_ps(sumA, sumA_1);
+		sumB = _mm_add_ps(sumB, sumB_1);
+
+		// horizontaly add sumA and sumB
+		_mm_store_ps(aux, _mm_hadd_ps(_mm_hadd_ps(sumA, sumB), _mm_setzero_ps()));
+
+		sumAtot = aux[0];
+		sumBtot = aux[1];
+
+		*res = sumAtot/sumBtot;
+	}
+}
+
+
 void smoothing_speedup(long long len,long long iters, double *timeVector){
 	float *x,*y,*res;
 	
@@ -455,6 +627,21 @@ void smoothing_speedup(long long len,long long iters, double *timeVector){
 	sprintf(name,"128_unroll4_%llu.csv",len);
 	write_results(name,x,y,res,len);
 
+
+	/* WARM UP CACHE FOR SSE128 unroll8 CASE */
+	sse128_smoothing_unroll8(x,y,res,len);
+
+	/* COMPUTE AVERAGE TIME FOR SSE128 unroll8 CASE */
+	tStart = PAPI_get_real_usec();
+	for (i=0; i<iters; i++)
+		sse128_smoothing_unroll8(x,y,res,len);
+	tEnd = PAPI_get_real_usec();
+	timeVector[5] = ((double)(tEnd - tStart)) / iters;
+
+	sprintf(name,"128_unroll8_%llu.csv",len);
+	write_results(name,x,y,res,len);
+
+
 	/* FREE ALLOCATED MEMORY */
 	aligned_free(x);
 	aligned_free(y);
@@ -465,7 +652,7 @@ void smoothing_speedup(long long len,long long iters, double *timeVector){
 
 int main(void) {
 
-	double timeVector[5];
+	double timeVector[6];
 	int retval;
 	long long i;
 	
@@ -479,17 +666,17 @@ int main(void) {
 	srand(time(NULL));
 
 	
-	printf("==========================================================================\n");
-	printf("   COMPUTING SMOOTHING SPEEDUP for SSE-128\n");
- 	printf("==========================================================================\n");
-	printf("| Vector |   Original |   speedup   |   speedup   |   speedup   |   speedup   |\n");
-	printf("| Length |  Time [us] |    simple   | unrolling 2 | unrolling 3 | unrolling 4 |\n");
-	printf("+--------+------------+-------------+-------------+-------------+-------------+\n");
+	puts("=============================================================================================");
+	puts("   COMPUTING SMOOTHING SPEEDUP for SSE-128\n");
+ 	puts("=============================================================================================");
+	puts("| Vector |   Original |   speedup   |   speedup   |   speedup   |   speedup   |   speedup   |");
+	puts("| Length |  Time [us] |    simple   | unrolling 2 | unrolling 3 | unrolling 4 | unrolling 8 |");
+	puts("+--------+------------+-------------+-------------+-------------+-------------+-------------+");
 	for (i=4;i<14; i++){
 		smoothing_speedup(1<<i, 5, timeVector);
-		printf("|  %5d | %10.1f | %11.6f | %11.6f | %11.6f | %11.6f |\n", 1<<i, timeVector[0], timeVector[0]/timeVector[1], timeVector[0]/timeVector[2], timeVector[0]/timeVector[3], timeVector[0]/timeVector[4]);
+		printf("|  %5d | %10.1f | %11.6f | %11.6f | %11.6f | %11.6f | %11.6f |\n", 1<<i, timeVector[0], timeVector[0]/timeVector[1], timeVector[0]/timeVector[2], timeVector[0]/timeVector[3], timeVector[0]/timeVector[4], timeVector[0]/timeVector[5]);
 	}
-	printf("+--------+------------+-------------+-------------+-------------+\n\n");
+	puts("+--------+------------+-------------+-------------+-------------+-------------+-------------+\n\n");
 
 
 	printf("Shuting down PAPI library...\n\n");
