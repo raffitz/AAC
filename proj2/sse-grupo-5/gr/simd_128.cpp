495f89241c844cfddc67230cd1be1ba74806a425
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-05-30 18:21:03 +0000

    Wrote unroll3.

--- proj2/sse-grupo-5/gr/simd_128.cpp
+++ proj2/sse-grupo-5/gr/simd_128.cpp
@@ -197,6 +197,84 @@ void sse128_smoothing_unroll2(float *x, float *y, float *res, int len)
 }
 
 
+void sse128_smoothing_unroll3(float *x, float *y, float *res, int len)
+{
+	float *xi, *xj, *yj;
+	float aux[4];
+	float sumAtot, sumBtot;
+
+	__m128 sumA, sumB, sumA_1, sumB_1, sumA_2, sumB_2;
+	__m128 divisor = _mm_div_ps(_mm_set_ps1(1.0), _mm_set_ps1(2*_SMOOTH*_SMOOTH));
+	__m128 exponential, exponential_1, exponential_2;
+	__m128 cache, cache_1, cache_2;
+
+	for(xi=x; xi < x+len; xi++, res++)
+	{
+		sumA = sumB = _mm_setzero_ps();
+		sumA_1 = sumB_1 = _mm_setzero_ps();
+		sumA_2 = sumB_2 = _mm_setzero_ps();
+
+		for(xj=x, yj=y; xj < x+len; xj+=12, yj+=12)
+		{
+			// e^[(-(xi-xj)^2) / (2*smoothing^2)]
+			cache = _mm_sub_ps(_mm_load_ps1(xi), _mm_load_ps(xj));
+			cache_1 = _mm_sub_ps(_mm_load_ps1(xi+4), _mm_load_ps(xj+4));
+			cache_2 = _mm_sub_ps(_mm_load_ps1(xi+8), _mm_load_ps(xj+8));
+			exponential = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache,
+								cache
+								)), divisor));
+
+			sumB = _mm_add_ps(sumB, exponential);
+			sumA = _mm_add_ps(sumA, _mm_mul_ps(_mm_load_ps(yj), exponential));
+
+
+
+			exponential_1 = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache_1,
+								cache_1
+								)), divisor));
+
+			sumB_1 = _mm_add_ps(sumB_1, exponential_1);
+			sumA_1 = _mm_add_ps(sumA_1, _mm_mul_ps(_mm_load_ps(yj+4), exponential_1));
+
+
+
+			exponential_2 = _mm_exp_ps(
+					_mm_mul_ps(
+						_mm_sub_ps(_mm_setzero_ps(),
+							_mm_mul_ps(
+								cache_2,
+								cache_2
+								)), divisor));
+
+			sumB_2 = _mm_add_ps(sumB_2, exponential_2);
+			sumA_2 = _mm_add_ps(sumA_2, _mm_mul_ps(_mm_load_ps(yj+8), exponential_2));
+		}
+
+		sumA = _mm_add_ps(sumA, sumA_1);
+		sumB = _mm_add_ps(sumB, sumB_1);
+
+		sumA = _mm_add_ps(sumA, sumA_2);
+		sumB = _mm_add_ps(sumB, sumB_2);
+
+		// horizontaly add sumA and sumB
+		_mm_store_ps(aux, _mm_hadd_ps(_mm_hadd_ps(sumA, sumB), _mm_setzero_ps()));
+
+		sumAtot = aux[0];
+		sumBtot = aux[1];
+
+		*res = sumAtot/sumBtot;
+	}
+}
+
+
 void sse128_smoothing_unroll4(float *x, float *y, float *res, int len)
 {
 	float *xi, *xj, *yj;
@@ -350,6 +428,20 @@ void smoothing_speedup(long long len,long long iters, double *timeVector){
 	write_results(name,x,y,res,len);
 	
 
+	/* WARM UP CACHE FOR SSE128 unroll3 CASE */
+	sse128_smoothing_unroll3(x,y,res,len);
+
+	/* COMPUTE AVERAGE TIME FOR SSE128 unroll3 CASE */
+	tStart = PAPI_get_real_usec();
+	for (i=0; i<iters; i++)
+		sse128_smoothing_unroll3(x,y,res,len);
+	tEnd = PAPI_get_real_usec();
+	timeVector[3] = ((double)(tEnd - tStart)) / iters;
+
+	sprintf(name,"128_unroll3_%llu.csv",len);
+	write_results(name,x,y,res,len);
+
+
 	/* WARM UP CACHE FOR SSE128 unroll4 CASE */
 	sse128_smoothing_unroll4(x,y,res,len);
 
@@ -358,7 +450,7 @@ void smoothing_speedup(long long len,long long iters, double *timeVector){
 	for (i=0; i<iters; i++)
 		sse128_smoothing_unroll4(x,y,res,len);
 	tEnd = PAPI_get_real_usec();
-	timeVector[3] = ((double)(tEnd - tStart)) / iters;
+	timeVector[4] = ((double)(tEnd - tStart)) / iters;
 
 	sprintf(name,"128_unroll4_%llu.csv",len);
 	write_results(name,x,y,res,len);
@@ -373,7 +465,7 @@ void smoothing_speedup(long long len,long long iters, double *timeVector){
 
 int main(void) {
 
-	double timeVector[4];
+	double timeVector[5];
 	int retval;
 	long long i;
 	
@@ -390,12 +482,12 @@ int main(void) {
 	printf("==========================================================================\n");
 	printf("   COMPUTING SMOOTHING SPEEDUP for SSE-128\n");
  	printf("==========================================================================\n");
-	printf("| Vector |   Original |   speedup   |   speedup   |   speedup   |\n");
-	printf("| Length |  Time [us] |    simple   | unrolling 2 | unrolling 4 |\n");
-	printf("+--------+------------+-------------+-------------+-------------+\n");
+	printf("| Vector |   Original |   speedup   |   speedup   |   speedup   |   speedup   |\n");
+	printf("| Length |  Time [us] |    simple   | unrolling 2 | unrolling 3 | unrolling 4 |\n");
+	printf("+--------+------------+-------------+-------------+-------------+-------------+\n");
 	for (i=4;i<14; i++){
 		smoothing_speedup(1<<i, 5, timeVector);
-		printf("|  %5d | %10.1f | %11.6f | %11.6f | %11.6f |\n", 1<<i, timeVector[0], timeVector[0]/timeVector[1], timeVector[0]/timeVector[2], timeVector[0]/timeVector[3]);
+		printf("|  %5d | %10.1f | %11.6f | %11.6f | %11.6f | %11.6f |\n", 1<<i, timeVector[0], timeVector[0]/timeVector[1], timeVector[0]/timeVector[2], timeVector[0]/timeVector[3], timeVector[0]/timeVector[4]);
 	}
 	printf("+--------+------------+-------------+-------------+-------------+\n\n");
 
